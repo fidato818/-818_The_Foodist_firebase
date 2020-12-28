2ec2454b886249b4af6fc922afcd073a6f51a570
Adnan Ahmed <adnanahmed818@gmail.com>
2020-12-28 12:15:11 +0500

    Update Data

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,7 @@
 import * as firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBe30BeWVTiG0SsWulSJ1zz0LCJhhNPQJA",
-  authDomain: "la-nourriture.firebaseapp.com",
-  databaseURL: "https://la-nourriture.firebaseio.com",
-  projectId: "la-nourriture",
-  storageBucket: "la-nourriture.appspot.com",
-  messagingSenderId: "108676779284",
-  appId: "1:108676779284:web:43a10e4073100d42"
+
 })
 
 // Initialize Firebase
